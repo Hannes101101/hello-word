b9f5aecf378a04323f7d72387311c6e480cfefa7
Hanne <hanne@gmail.com>
2018-10-13 17:48:03 +0200

    removed some system outs

--- MyClass.java
+++ MyClass.java
@@ -2,10 +2,6 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		
-		System.out.println("foobar");
-		
-		System.out.println("blabla");
 	}
   
 }
