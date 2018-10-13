19f7b030fae32a778bc5ec50a52675071f9e0e80
Hanne <hanne@gmail.com>
2018-10-13 17:35:20 +0200

    added a new system out

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,9 @@
 public class MyClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");		
+		System.out.println("Hello World");
+		
+		System.out.println("foobar");
 	}
   
 }
